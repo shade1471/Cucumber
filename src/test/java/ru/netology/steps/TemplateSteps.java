fc19491c8e200ab470a7bf01d28db1695d3bf254
Andrey Gribanov <shade1471@gmail.com>
2022-03-03 11:32:52 +0700

    Edit tests and scenarios

--- src/test/java/ru/netology/steps/TemplateSteps.java
+++ src/test/java/ru/netology/steps/TemplateSteps.java
@@ -8,19 +8,23 @@ import io.cucumber.java.ru.Тогда;
 import ru.netology.data.DataHelper;
 import ru.netology.page.DashboardPage;
 import ru.netology.page.LoginPage;
+import ru.netology.page.RefillPage;
 import ru.netology.page.VerificationPage;
 
-import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 public class TemplateSteps {
+    private static RefillPage refillPage;
     private static LoginPage loginPage;
     private static DashboardPage dashboardPage;
     private static VerificationPage verificationPage;
+
     private DataHelper.AuthInfo authInfo = DataHelper.getAuthInfo();
     private DataHelper.AuthInfo authInfoInvalid = DataHelper.getInvalidAuthInfo(authInfo);
-    private DataHelper.Card cardNumber = DataHelper.getNumberCard(authInfo);
     private DataHelper.VerificationCode verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+    private String cardOneId = "92df3f1c-a033-48e6-8390-206f6b1f56c0";
+    private String cardTwoId = "0f3f5c2a-249e-4c3d-8287-09f7a039391d";
 
     @Пусть("открыта страница с формой авторизации {string}")
     public void openAuthPage(String url) {
@@ -42,5 +46,18 @@ public class TemplateSteps {
         dashboardPage.verifyIsDashboardPage();
     }
 
+    @И("пользователь переводит {string} рублей с карты с номером {string} на свою 1 карту с главной страницы")
+    public void increaseBalanceFirstCard(String amount, String from) {
+        dashboardPage.refresh(cardOneId, cardTwoId, authInfo);
+        refillPage = dashboardPage.increaseBalance(cardOneId);
+        refillPage.refillCard(amount, from);
+    }
+
+    @Тогда("баланс его 1 карты из списка на главной странице должен стать {int} рублей")
+    public void getBalance(int expectedBalance) {
+        int actualBalance = dashboardPage.getCardBalance(cardOneId);
+        assertEquals(expectedBalance, actualBalance);
+    }
 
 }
+
